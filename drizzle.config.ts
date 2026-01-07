530ec354fd4d2ccc1015b04c59d40ad3581ee1de
Taofeek Lateef <lateef4ril32@gmail.com>
2026-01-07 01:58:41 +0100

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,13 @@
-import { defineConfig } from "drizzle-kit";
+import type { Config } from "drizzle-kit";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
-// drizzle.config.ts
-// import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
-  dialect: "postgresql",
+export default {
+  schema: "./db/schema.ts",
+  out: "./drizzle",
+  driver: "pg",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL!,
   },
-});
+} satisfies Config;
