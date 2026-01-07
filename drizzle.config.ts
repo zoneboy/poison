9bcda42fc0454eeb95ea7207a329a6e48370b3e9
Taofeek Lateef <lateef4ril32@gmail.com>
2026-01-07 02:04:46 +0100

    uo

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,8 +6,8 @@ dotenv.config();
 export default {
   schema: "./db/schema.ts",
   out: "./drizzle",
-  driver: "pg",
+  dialect: "postgresql",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
   },
 } satisfies Config;
